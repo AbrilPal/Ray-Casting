ebccc699310ee64db54662a53d71df0a8cca16e1
Abril Palencia <pal18198@uvg.edu.gt>
2020-11-08 22:53:19 -0600

    enemigos y pantallas

--- RayCaster.py
+++ RayCaster.py
@@ -1,233 +1,297 @@
 # Andrea Abril Palencia Gutierrez, 18198
 # RC2: Textures --- Graficas por computadora, seccion 20
-
 import pygame
+from math import pi, cos, sin, atan2
 
-from math import cos, sin, pi
-# Si estan en C++, pueden utilizar SDL
 
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-BACKGROUND = (250, 251, 246)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BACKGROUND = (0, 255, 255)
+
+
+wall1 = pygame.image.load('./marmol.jpg')
+wall2 = pygame.image.load('./marmol2.jpg')
+wall3 = pygame.image.load('./diamond.png')
+wall4 = pygame.image.load('./lava.png')
 
 textures = {
-    '1' : pygame.image.load('grama.jpg'),
-    '2' : pygame.image.load('madera.jpg'),
-    '3' : pygame.image.load('piedra.png'),
-    '4' : pygame.image.load('puertas.jpg')
-    }
+  "1": wall1,
+  "2": wall2,
+  "3": wall3,
+  "4": wall4,
+}
+
+item = pygame.image.load('./player.png')
+
+enemies = [
+  {
+    "x": 100,
+    "y": 200,
+    "texture": pygame.image.load('./pulpo.png')
+  },
+  {
+    "x": 280,
+    "y": 190,
+    "texture": pygame.image.load('./pulpo.png')
+  },
+  {
+    "x": 225,
+    "y": 340,
+    "texture": pygame.image.load('./pulpo.png')
+  },
+  {
+    "x": 405,
+    "y": 275,
+    "texture": pygame.image.load('./diamante.png')
+  }
+]
 
 class Raycaster(object):
-    def __init__(self,screen):
-        self.screen = screen
-        _, _, self.width, self.height = screen.get_rect()
+  def __init__(self, screen):
+    _, _, self.width, self.height = screen.get_rect()
+    self.screen = screen
+    self.blocksize = 50
+    self.player = {
+      "x": self.blocksize + 20,
+      "y": self.blocksize + 20,
+      "a": 0,
+      "fov": pi/3
+    }
+    self.map = []
+    self.zbuffer = [-float('inf') for z in range(0, 500)]
+    # self.clear()
 
-        self.map = []
-        self.blocksize = 50
-        self.wallHeight = 50
+  def clear(self):
+    for x in range(self.width):
+      for y in range(self.height):
+        r = int((x/self.width)*255) if x/self.width < 1 else 1
+        g = int((y/self.height)*255) if y/self.height < 1 else 1
+        b = 0
+        color = (r, g, b)
+        self.point(x, y, color)
 
-        self.stepSize = 5
+  def point(self, x, y, c = None):
+    screen.set_at((x, y), c)
 
-        self.setColor(WHITE)
+  def draw_rectangle(self, x, y, texture):
+    for cx in range(x, x + 50):
+      for cy in range(y, y + 50):
+        tx = int((cx - x)*173 / 50)
+        ty = int((cy - y)*173 / 50)
+        c = texture.get_at((tx, ty))
+        self.point(cx, cy, c)
 
-        self.player = {
-            "x" : 75,
-            "y" : 175,
-            "angle" : 0,
-            "fov" : 60
-            }
+  def load_map(self, filename):
+    with open(filename) as f:
+      for line in f.readlines():
+        self.map.append(list(line))
 
-    def setColor(self, color):
-        self.blockColor = color
+  def cast_ray(self, a):
+    d = 0
+    while True:
+      x = self.player["x"] + d*cos(a)
+      y = self.player["y"] + d*sin(a)
 
-    def load_map(self, filename):
-        with open(filename) as f:
-            for line in f.readlines():
-                self.map.append(list(line))
+      i = int(x/50)
+      j = int(y/50)
 
-    def drawRect(self, x, y, tex):
-        tex = pygame.transform.scale(tex, (self.blocksize, self.blocksize))
-        rect = tex.get_rect()
-        rect = rect.move( (x,y) )
-        self.screen.blit(tex, rect)
+      if self.map[j][i] != ' ':
+        hitx = x - i*50
+        hity = y - j*50
 
-    def drawPlayerIcon(self,color):
+        if 1 < hitx < 49:
+          maxhit = hitx
+        else:
+          maxhit = hity
 
-        rect = (int(self.player['x'] - 2), int(self.player['y'] - 2), 5, 5)
-        self.screen.fill(color, rect)
+        tx = int(maxhit * 173 / 50)
 
-    def castRay(self, a):
-        rads = a * pi / 180
-        dist = 0
-        while True:
-            x = int(self.player['x'] + dist * cos(rads))
-            y = int(self.player['y'] + dist * sin(rads))
+        return d, self.map[j][i], tx
 
-            i = int(x/self.blocksize)
-            j = int(y/self.blocksize)
+      self.point(int(x), int(y), (255, 255, 255))
 
-            if self.map[j][i] != ' ':
-                hitX = x - i*self.blocksize
-                hitY = y - j*self.blocksize
+      d += 1
 
-                if 1 < hitX < self.blocksize - 1:
-                    maxHit = hitX
-                else:
-                    maxHit = hitY
+  def draw_stake(self, x, h, texture, tx):
+    start = int(250 - h/2)
+    end = int(250 + h/2)
+    for y in range(start, end):
+      ty = int(((y - start)*173)/(end - start))
+      c = texture.get_at((tx, ty))
+      self.point(x, y, c)
 
-                tx = maxHit / self.blocksize
+  def draw_sprite(self, sprite):
+    sprite_a = atan2(sprite["y"] - self.player["y"], sprite["x"] - self.player["x"])
 
-                return dist, self.map[j][i], tx
+    sprite_d = ((self.player["x"] - sprite["x"])**2 + (self.player["y"] - sprite["y"])**2)**0.5
+    sprite_size = (500/sprite_d) * 70
 
-            self.screen.set_at((x,y), WHITE)
+    sprite_x = 500 + (sprite_a - self.player["a"])*500/self.player["fov"] + 250 - sprite_size/2
+    sprite_y = 250 - sprite_size/2
 
-            dist += 1
+    sprite_x = int(sprite_x)
+    sprite_y = int(sprite_y)
+    sprite_size = int(sprite_size)
 
-    def render(self):
+    for x in range(sprite_x, sprite_x + sprite_size):
+      for y in range(sprite_y, sprite_y + sprite_size):
+        if 500 < x < 1000 and self.zbuffer[x - 500] >= sprite_d:
+          tx = int((x - sprite_x) * 128/sprite_size)
+          ty = int((y - sprite_y) * 128/sprite_size)
+          c = sprite["texture"].get_at((tx, ty))
+          if c != (152, 0, 136, 255):
+            self.point(x, y, c)
+            self.zbuffer[x - 500] = sprite_d
 
-        halfWidth = int(self.width / 2)
-        halfHeight = int(self.height / 2)
+  def draw_item(self, xi, yi, w = 200, h = 200):
+    for x in range(xi, xi + w):
+      for y in range(yi, yi + h):
+        tx = int((x - xi) * 32/w)
+        ty = int((y - yi) * 32/h)
+        c = item.get_at((tx, ty))
+        if c != (152, 0, 136, 255):
+          self.point(x, y, c)
 
-        for x in range(0, halfWidth, self.blocksize):
-            for y in range(0, self.height, self.blocksize):
-                
-                i = int(x/self.blocksize)
-                j = int(y/self.blocksize)
+  def render(self):
+    for x in range(0, 500, 50):
+      for y in range(0, 500, 50):
+        i = int(x/50)
+        j = int(y/50)
+        if self.map[j][i] != ' ':
+          self.draw_rectangle(x, y, textures[self.map[j][i]])
 
-                if self.map[j][i] != ' ':
-                    self.drawRect(x, y, textures[self.map[j][i]])
+    self.point(self.player["x"], self.player["y"], (255, 255, 255))
 
-        self.drawPlayerIcon(BLACK)
+    for i in range(0, 500):
+      self.point(500, i, (0, 0, 0))
+      self.point(501, i, (0, 0, 0))
+      self.point(499, i, (0, 0, 0))
 
-        for i in range(halfWidth):
-            angle = self.player['angle'] - self.player['fov'] / 2 + self.player['fov'] * i / halfWidth
-            dist, wallType, tx = self.castRay(angle)
+    for i in range(0, 500):
+      a =  self.player["a"] - self.player["fov"]/2 + self.player["fov"]*i/500
+      d, c, tx = self.cast_ray(a)
+      x = 500 + i
+      h = 500/(d*cos(a-self.player["a"])) * 70
+      self.draw_stake(x, h, textures[c], tx)
+      self.zbuffer[i] = d
 
-            x = halfWidth + i 
+    for enemy in enemies:
+      self.point(enemy["x"], enemy["y"], (0, 0, 0))
+      self.draw_sprite(enemy)
 
-            # perceivedHeight = screenHeight / (distance * cos( rayAngle - viewAngle) * wallHeight
-            h = self.height / (dist * cos( (angle - self.player['angle']) * pi / 180 )) * self.wallHeight
+    self.draw_item(1000-300-128,500-200)
 
-            start = int( halfHeight - h/2)
-            end = int( halfHeight + h/2)
-
-            img = textures[wallType]
-            tx = int(tx * img.get_width())
-
-            for y in range(start, end):
-                ty = (y - start) / (end - start)
-                ty = int(ty * img.get_height())
-                texColor = img.get_at((tx, ty))
-                self.screen.set_at((x, y), texColor)
-
-        for i in range(self.height):
-            self.screen.set_at( (halfWidth, i), BLACK)
-            self.screen.set_at( (halfWidth+1, i), BLACK)
-            self.screen.set_at( (halfWidth-1, i), BLACK)
 
 
 pygame.init()
-screen = pygame.display.set_mode((1000,500), pygame.DOUBLEBUF | pygame.HWACCEL) #, pygame.FULLSCREEN)
+
+def text_objects(text, font):
+  textSurface = font.render(text, True, (0,0,0))
+  return textSurface, textSurface.get_rect()
+
+def button(msg,x,y,w,h,ic,ac, action=None):
+  mouse = pygame.mouse.get_pos()
+  click = pygame.mouse.get_pressed()
+  if x+w > mouse[0] > x and y+h > mouse[1] > y:
+      pygame.draw.rect(screen, ac,(x,y,w,h))
+
+      if click[0] == 1 and action != None:
+        if action == 'play':
+          game()
+        elif action == 'quit':
+          pygame.quit()
+          quit()
+
+  else:
+      pygame.draw.rect(screen, ic,(x,y,w,h))
+
+  smallText = pygame.font.Font("freesansbold.ttf",20)
+  textSurf, textRect = text_objects(msg, smallText)
+  textRect.center = ( (x+(w/2)), (y+(h/2)) )
+  screen.blit(textSurf, textRect)
+    
+
+screen = pygame.display.set_mode((1000, 500))
 screen.set_alpha(None)
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial", 30)
-
-def updateFPS():
-    fps = str(int(clock.get_fps()))
-    fps = font.render(fps, 1, pygame.Color("white"))
-    return fps
-
 r = Raycaster(screen)
+r.load_map('./map.txt')
 
-r.setColor( (128,0,0) )
-r.load_map('mapa.txt')
+def gameWin():
+  intro = True
 
-isRunning = True
+  while intro:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+        pygame.quit()
+        quit()
 
-while isRunning:
+    screen.fill((249,253,250))
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    TextSurf, TextRect = text_objects('Victoria! ♥', largeText)
+    TextRect.center = ((1000/2),(500/2))
+    screen.blit(TextSurf, TextRect)
 
-    for ev in pygame.event.get():
-        if ev.type == pygame.QUIT:
-            isRunning = False
-        
-        newX = r.player['x']
-        newY = r.player['y']
-        
-        if ev.type == pygame.KEYDOWN:
-            if ev.key == pygame.K_ESCAPE:
-                isRunning = False
-            elif ev.key == pygame.K_w:
-                    newX = r.player['x'] + cos(r.player['angle'] * pi / 180) * r.stepSize
-                    newY = r.player['y'] + sin(r.player['angle'] * pi / 180) * r.stepSize
+    button('Quit',450,400,100,50,(227, 176, 252),(227, 176, 252),'quit')
 
-                    i = int(newX/r.blocksize)
-                    j = int(newY/r.blocksize)
-
-                    if r.map[j][i] ==' ':
-                        r.player['x'] = newX
-                        r.player['y'] = newY
-                    
-            elif ev.key == pygame.K_s:
-                    newX = r.player['x'] - cos(r.player['angle'] * pi / 180) * r.stepSize
-                    newY = r.player['y'] - sin(r.player['angle'] * pi / 180) * r.stepSize
-
-                    i = int(newX/r.blocksize)
-                    j = int(newY/r.blocksize)
-
-                    if r.map[j][i] ==' ':
-                        r.player['x'] = newX
-                        r.player['y'] = newY
-
-            elif ev.key == pygame.K_a:
-                    newX = r.player['x'] - cos((r.player['angle'] + 90) * pi / 180) * r.stepSize
-                    newY = r.player['y'] - sin((r.player['angle'] + 90) * pi / 180) * r.stepSize
-
-                    i = int(newX/r.blocksize)
-                    j = int(newY/r.blocksize)
-
-                    if r.map[j][i] ==' ':
-                        r.player['x'] = newX
-                        r.player['y'] = newY
-
-            elif ev.key == pygame.K_d:
-                    newX = r.player['x'] + cos((r.player['angle'] + 90) * pi / 180) * r.stepSize
-                    newY = r.player['y'] + sin((r.player['angle'] + 90) * pi / 180) * r.stepSize
-
-                    i = int(newX/r.blocksize)
-                    j = int(newY/r.blocksize)
-
-                    if r.map[j][i] ==' ':
-                        r.player['x'] = newX
-                        r.player['y'] = newY
-
-            elif ev.key == pygame.K_q:
-                    r.player['angle'] -= 5
-            elif ev.key == pygame.K_e:
-                    r.player['angle'] -= 5
-
-
-            i = int(newX / r.blocksize)
-            j = int(newY / r.blocksize)
-
-            if r.map[j][i] == ' ':
-                r.player['x'] = newX
-                r.player['y'] = newY
-
-    screen.fill(pygame.Color("gray")) #Fondo
-
-    #Techo
-    screen.fill(pygame.Color("dimgray"), (int(r.width / 2), 0, int(r.width / 2),int(r.height / 2)))
-    
-    #Piso
-    screen.fill(pygame.Color("dimgray"), (int(r.width / 2), int(r.height / 2), int(r.width / 2),int(r.height / 2)))
-
-    r.render()
-    
-    # FPS
-    screen.fill(pygame.Color("black"), (0,0,30,30))
-    screen.blit(updateFPS(), (0,0))
-    clock.tick(30)  
-    
     pygame.display.update()
 
-pygame.quit()
+def gameIntro():
+  intro = True
+
+  while intro:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+        pygame.quit()
+        quit()
+
+    screen.fill((251, 214, 250))
+    largeText = pygame.font.Font('freesansbold.ttf', 90)
+    TextSurf, TextRect = text_objects('PULPITO', largeText)
+    TextRect.center = ((1000/2),(100))
+    screen.blit(TextSurf, TextRect)
+
+    largeText = pygame.font.Font('freesansbold.ttf', 30)
+    TextSurf, TextRect = text_objects('Encuentra el diamante', largeText)
+    TextRect.center = ((1000/2),(250))
+    screen.blit(TextSurf, TextRect)
+
+    button('Comenzar',450,400,100,50,(227, 176, 252),(227, 176, 252),'play')
+
+    pygame.display.update()
+
+def game():
+  c = 0
+  jugar = True
+  while jugar:
+    screen.fill((113, 113, 113))
+    r.render()
+
+    for e in pygame.event.get():
+      if e.type == pygame.QUIT or (e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE):
+        exit(0)
+      if e.type == pygame.KEYDOWN:
+        if e.key == pygame.K_a:
+          r.player["a"] -= pi/10
+        elif e.key == pygame.K_d:
+          r.player["a"] += pi/10
+
+        elif e.key == pygame.K_RIGHT:
+          r.player["x"] += 10
+        elif e.key == pygame.K_LEFT:
+          r.player["x"] -= 10
+        elif e.key == pygame.K_UP:
+          r.player["y"] -= 10
+        elif e.key == pygame.K_DOWN:
+          r.player["y"] += 10
+
+        if e.key == pygame.K_f:
+          if screen.get_flags() and pygame.FULLSCREEN:
+              pygame.display.set_mode((1000, 500))
+          else:
+              pygame.display.set_mode((1000, 500),  pygame.DOUBLEBUF|pygame.HWACCEL|pygame.FULLSCREEN)
+      if r.player['x'] > 398 and r.player['x'] < 451 and r.player['y'] > 252 and r.player['y'] < 298:
+        gameWin()
+
+    pygame.display.flip()
+
+gameIntro()
